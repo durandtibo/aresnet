d0695e060c00cbc679e7bfed8c9c6a31684edd7d
Thibaut Durand <durand.tibo@gmail.com>
2026-01-28 13:23:47 -0800

    Add dev tools (#4)

---
+++ dev/generate_versions.py
@@ -0,0 +1,46 @@
+# noqa: INP001
+r"""Script to create or update the package versions."""
+
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+
+from feu.utils.io import save_json
+from feu.version import (
+    fetch_latest_major_versions,
+    fetch_latest_minor_versions,
+    filter_every_n_versions,
+    filter_last_n_versions,
+    sort_versions,
+    unique_versions,
+)
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def fetch_package_versions() -> dict[str, list[str]]:
+    r"""Get the versions for each package.
+
+    Returns:
+        A dictionary with the versions for each package.
+    """
+    return {
+        # Data structure dependencies
+        "coola": list(fetch_latest_minor_versions("coola", lower="1.0")),
+        "httpx": list(fetch_latest_minor_versions("httpx", lower="0.28")),
+    }
+
+
+def main() -> None:
+    r"""Generate the package versions and save them in a JSON file."""
+    versions = fetch_package_versions()
+    logger.info(f"{versions=}")
+    path = Path(__file__).parent.parent.joinpath("dev/config").joinpath("package_versions.json")
+    logger.info(f"Saving package versions to {path}")
+    save_json(versions, path, exist_ok=True)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
